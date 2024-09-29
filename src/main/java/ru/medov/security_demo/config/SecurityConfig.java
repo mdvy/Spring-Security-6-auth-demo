64479f9d79a4c1af6b27d1d56c551bb4a70c0508
mdvy <9508923633@ya.ru>
2024-09-29 13:45:47 +0300

    cleanup

--- src/main/java/ru/medov/security_demo/config/SecurityConfig.java
+++ src/main/java/ru/medov/security_demo/config/SecurityConfig.java
@@ -5,25 +5,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import ru.medov.security_demo.domain.Permission;
-import ru.medov.security_demo.domain.Role;
 import ru.medov.security_demo.security.JwtTokenFilter;
 
 
@@ -47,37 +39,13 @@ public class SecurityConfig {
         return
                 httpSecurity
                         .csrf(AbstractHttpConfigurer::disable)
-                        .authorizeHttpRequests(auth->auth
+                        .authorizeHttpRequests(auth -> auth
                                 .requestMatchers("/", "/api/v1/auth/**").permitAll()
-
-
-//                                .requestMatchers(GET, "/api/v1/users/**").hasAuthority(Permission.USER_READ.getPermission())
-//                                .requestMatchers(POST, "/api/v1/users/**").hasAuthority(Permission.USER_WRITE.getPermission())
-//                                .requestMatchers(DELETE, "/api/v1/users/**").hasAuthority(Permission.USER_WRITE.getPermission())
                                 .anyRequest().authenticated())
-//                        .formLogin(auth -> auth
-//                                        .loginPage("/auth/login")
-//                                        .defaultSuccessUrl("/auth/success"))
-//                        .httpBasic(Customizer.withDefaults())
-//                        .logout(logout -> logout
-//                                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout","POST"))
-//                                .invalidateHttpSession(true)
-//                                .clearAuthentication(true)
-//                                .deleteCookies("JSESSIONID")
-//                                .logoutSuccessUrl("/auth/"))
                         .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                         .sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .build();
     }
-
-
-//    @Bean
-//    public AuthenticationProvider authenticationProvider(){
-//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setPasswordEncoder(passwordEncoder());
-//        authenticationProvider.setUserDetailsService(userDetailsService);
-//        return authenticationProvider;
-//    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
@@ -86,12 +54,12 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(5);
     }
 
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
